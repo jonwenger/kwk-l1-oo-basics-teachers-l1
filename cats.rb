9a71eb184393bc36bf3508dd47e84a2451e61c0e
Jon Wenger <jonwenger783@gmail.com>
2018-08-10 19:28:17 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -10,14 +10,6 @@ class Cats
 end
 
 tiger = Cats.new("very", "pretty fuzzy", "super duper mean")
-# puts "is the tiger cute? #{tiger.is_it_cute}"
-
-garfield = Cats.new("has a bit of an attidude", "up to debate", "yes, he's hangry all the time")
-# puts "is garfield cute? #{garfield.is_it_cute}"
-
-puts "is tiger mean? #{tiger.is_it_mean} <<<<>>>> #{tiger.basic_facts}"
-puts "is garifeld mean? #{garifeld.is_it_mean} :) :) :)  #{basic_facts}"
-
 
 
 
